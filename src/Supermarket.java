6a4a65b56ba73414b07eca685fb327914ff6de20
yumikaamemiya <yumikaamemiya@172.16.20.133>
2017-12-08 11:39:05 -0500

    Sets up classes

---
+++ src/Supermarket.java
@@ -0,0 +1,12 @@
+
+public class Supermarket {
+
+	public static void main(String[] args) {
+		//create an instance of a class
+		//create a can of soup
+		//make a flower arrangement
+		//ring up total price
+
+	}
+
+}
